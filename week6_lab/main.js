041f1ac1c508fe6385ab157fd62ec3c2720aa8e5
Tiffany Liu <tiffany.liu9898@gmail.com>
2018-10-01 11:48:13 -0400

    lab 6

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -1,6 +1,56 @@
-// represent adding new list to ToDo app, currently just creates an alert message and print to console whenever function is called
-
 function addNewList() {
-	alert('hello world alert!');
-	console.log('hello world console');
+    alert('hello world!');
 }
+
+function addListItem() {
+    console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+	// remove li element (item) from ol element (item.parentNode)
+	item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+	if (item.checked) {// true if the input checkbox is checked
+		item.parentNode.style.textDecoration = "line-through";
+		// in css, this would be: "text-decoration: line-through"
+	} else {
+		item.parentNode.style.textDecoration = "none";
+		// in css, this would be "text-decoration: none"
+	}
+}
+
+/*$(document).ready(function(){
+    $("#add-item").click(function() { // bind handler for click event
+    	console.log('hello')
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+	  // append the input value within an li element
+	  list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>")
+    });
+
+    $(".delete-item").click(function() {
+    	$(this).parent().remove();
+    	console.log("test");
+	});
+
+});*/
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
+$(document).on("click", "#complete-item")
+
