89391e4faa4ee36b79941906cbd786de323ce7ef
Tiffany Liu <tiffany.liu9898@gmail.com>
2018-10-01 10:46:41 -0400

    First commit for lab 6

---
+++ week6_lab/main.js
@@ -0,0 +1,6 @@
+// represent adding new list to ToDo app, currently just creates an alert message and print to console whenever function is called
+
+function addNewList() {
+	alert('hello world alert!');
+	console.log('hello world console');
+}
